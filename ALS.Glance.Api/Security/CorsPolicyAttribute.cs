d1cc2ed281eee4e057b39fc2273e33a843078257
Pedro Cardoso <smeegoan@gmail.com>
2015-04-12 23:57:04 +0100

    Added Cors to apphb.com

--- ALS.Glance.Api/Security/CorsPolicyAttribute.cs
+++ ALS.Glance.Api/Security/CorsPolicyAttribute.cs
@@ -23,6 +23,7 @@ namespace ALS.Glance.Api.Security
 
             // Add allowed origins.
             _policy.Origins.Add("http://localhost:53130");
+            _policy.Origins.Add("http://als-glance-web.apphb.com");
         }
 
         public Task<CorsPolicy> GetCorsPolicyAsync(HttpRequestMessage request,CancellationToken ct)
