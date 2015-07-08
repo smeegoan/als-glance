cff3fd04dcd5bb45b7f3dbc90b5161cba81cff21
Pedro Cardoso <smeegoan@gmail.com>
2015-07-08 18:09:38 +0100

    Bug fixes

--- ALS.Glance.Web/Scripts/alsglance.js
+++ ALS.Glance.Web/Scripts/alsglance.js
@@ -336,7 +336,8 @@ alsglance.dashboard.patients = alsglance.dashboard.patients || {
         var numberFormat = d3.format('.5f');
         data = data["value"];
         data.forEach(function (d) {
-            var date = new Date(d.BornOn);
+            var date = new Date();
+            date.setISO8601(d.BornOn);
             d.Age = moment().diff(date, 'years');
             d.Resume = '<a id="resume" href="javascript:void(0);" onclick="javascript:alsglance.presentation.showLoadingDialog.show(\'' + d.Name + '\');window.location=\'' + alsglance.resources.patientLink + d.Id + '\'" data-step="3" data-intro="' + alsglance.resources.patientsTip3 + '" data-position=\'left\'> <span id="resume" class="fa fa-eye"></span></a>';
         });
@@ -712,7 +713,8 @@ alsglance.dashboard.patient = alsglance.dashboard.patient || {
         var hourFormat = d3.format('.0f');
 
         data.forEach(function (d) {
-            var date = new Date(d.DateDate);
+            var date = new Date();
+            date.setISO8601(d.DateDate);
             d.DateDate = date;
             d.DateMonthInYear = d3.time.month(d.DateDate); // pre-calculate month for better performance
 
@@ -1003,3 +1005,41 @@ $(function () {
     }
     $('#version').text("(crossfilter: " + crossfilter.version + ", d3: " + d3.version + ", dc: " + dc.version + (window.Dygraph != null ? ", dygraph: " + Dygraph.VERSION : "") + ", moment: " + moment.version + ")");
 });
+
+//required for IE and FireFox
+Date.prototype.setISO8601 = function (string) {
+    var regexp = "([0-9]{4})(-([0-9]{2})(-([0-9]{2})" +
+        "(T([0-9]{2}):([0-9]{2})(:([0-9]{2})(\.([0-9]+))?)?" +
+        "(Z|(([-+])([0-9]{2}):([0-9]{2})))?)?)?)?";
+    var d = string.match(new RegExp(regexp));
+
+    var offset = 0;
+    var date = new Date(d[1], 0, 1);
+
+    if (d[3]) {
+        date.setMonth(d[3] - 1);
+    }
+    if (d[5]) {
+        date.setDate(d[5]);
+    }
+    if (d[7]) {
+        date.setHours(d[7]);
+    }
+    if (d[8]) {
+        date.setMinutes(d[8]);
+    }
+    if (d[10]) {
+        date.setSeconds(d[10]);
+    }
+    if (d[12]) {
+        date.setMilliseconds(Number("0." + d[12]) * 1000);
+    }
+    if (d[14]) {
+        offset = (Number(d[16]) * 60) + Number(d[17]);
+        offset *= ((d[15] == '-') ? 1 : -1);
+    }
+
+    offset -= date.getTimezoneOffset();
+    var time = (Number(date) + (offset * 60 * 1000));
+    this.setTime(Number(time));
+}
